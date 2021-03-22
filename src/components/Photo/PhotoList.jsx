463532b75882c3d1a1cc1fa05b72c320230d4a60
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2021-03-22 13:56:50 +0100

    Added css for grid, 4 images per row

--- src/components/Photo/PhotoList.jsx
+++ src/components/Photo/PhotoList.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import PhotoListDetail from './PhotoListDetail';
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles({
+	container: {
+		width: "90%",
+		display: 'grid',
+		gridTemplateColumns: 'repeat(4, 1fr)',
+		gap: '1em',
+		gridAutoRows: 'minmax(100px, auto)',
+	},
+});
 
 const PhotoList = ({ images }) => {
+	const classes = useStyles();
 	return (
-		<div className="photo-list-wrapper">
+		<div className={classes.container}>
 			{images.length > 0
 				? images.map((image) => (
-						<PhotoListDetail 
-						key={image.id} 
-						urls={image.urls.raw} 
-						description={image.description} 
+						<PhotoListDetail
+							className={classes.imageX}
+							key={image.id}
+							urls={image.urls.raw}
+							description={image.description}
 						/>
 				  ))
 				: []}
