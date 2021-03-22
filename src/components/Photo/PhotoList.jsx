a56b220957849c81c9bafd16bdf81d0151040564
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2021-03-22 16:49:18 +0100

    added media queries for grid

--- src/components/Photo/PhotoList.jsx
+++ src/components/Photo/PhotoList.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 import PhotoListDetail from './PhotoListDetail';
 import { createUseStyles } from 'react-jss';
 
+const maxWidth = 700;
 const useStyles = createUseStyles({
 	grid: {
 		display: 'grid',
 		gridTemplateColumns: 'repeat(4, 4fr)',
-		/* gridTemplateRows: 'repeat(8, 20vw)', */
+		gridTemplateRows: 'repeat(20vw)',
 		gridGap: '15px',
+	},
+	[`@media (max-width: ${maxWidth}px)`]: {
+		grid: {
+			gridTemplateColumns: 'repeat(1, 4fr)',
+		},
 	},
 });
 
