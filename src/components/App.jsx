1a037c4db7f2a4812ec90c15968279f11e558425
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2021-03-23 14:17:34 +0100

    Implementing infinite scroll

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import unsplash from '../api/unsplash';
 import Photo from './Photo/Photo';
 import { createUseStyles } from 'react-jss';
@@ -6,20 +7,21 @@ import { createUseStyles } from 'react-jss';
 const useStyles = createUseStyles({
 	container: {
 		margin: 'auto',
-		padding: '2em'
+		padding: '2em',
 	},
 });
 
 function App() {
 	const classes = useStyles();
 	const [images, setImages] = useState([]);
+	const [loaded, setIsLoaded] = useState(false);
 
 	const getPhotosFromUnsplash = async () => {
 		try {
 			const response = await unsplash.get('photos');
 			if (response.status === 200) {
-				setImages(response.data);
-				console.log(response.data);
+				setImages(() => [...images, ...response.data]);
+				setIsLoaded(true);
 			}
 		} catch (error) {
 			console.log(error);
@@ -33,7 +35,19 @@ function App() {
 
 	return (
 		<div className={classes.container}>
-			<Photo images={images} />
+			<InfiniteScroll
+				dataLength={images}
+				next={() => getPhotosFromUnsplash()}
+				hasMore={true}
+				loader={
+					<img
+						src="https://res.cloudinary.com/chuloo/image/upload/v1550093026/scotch-logo-gif_jq4tgr.gif"
+						alt="loading"
+					/>
+				}
+			>
+				{loaded ? <Photo images={images} /> : ''}
+			</InfiniteScroll>
 		</div>
 	);
 }
