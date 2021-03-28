c8a34d057759d5bc5e4a514b00634c15b7de315f
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2021-03-28 23:54:54 +0200

    second try with path

--- index.js
+++ index.js
@@ -6,13 +6,11 @@ app.use(cors());
 
 const port = process.env.PORT || 4000;
 
-app.use(express.static('client/build'));
+let root = path.join(__dirname, 'client/build/');
+app.use(express.static(root));
 
-// Express will serve up the index.html file
-// if it doesn't recognize the route
-const path = require('path');
-app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+ })
 
 app.listen(port, () => console.log(`App is live on port ${port}!`));
