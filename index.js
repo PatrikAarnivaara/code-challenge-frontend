e289e720ba6c3464b0c1395044622ffa4e158810
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2021-03-28 23:49:25 +0200

    added path to build

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const port = process.env.PORT || 4000;
 /* let root = path.join(__dirname, 'client/build/');
 app.use(express.static(root)); */
 
-app.get('/', (req, res) => {
-	res.send({ exampleMessage: 'REacT!' });
-});
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+ })
 
 app.listen(port, () => console.log(`App is live on port ${port}!`));
