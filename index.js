8914ad8dc222dce77f963805954001fa04bc1460
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2021-03-28 23:52:38 +0200

    changed path

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const app = express();
 app.use(cors());
 
 const port = process.env.PORT || 4000;
-/* let root = path.join(__dirname, 'client/build/');
-app.use(express.static(root)); */
 
-app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname, '/client/build/index.html'));
- })
+app.use(express.static('client/build'));
+
+// Express will serve up the index.html file
+// if it doesn't recognize the route
+const path = require('path');
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 app.listen(port, () => console.log(`App is live on port ${port}!`));
