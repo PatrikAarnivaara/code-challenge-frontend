3f5be73968a3a20b9c5749d9f0a4c855e7820cae
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2021-03-29 09:56:24 +0200

    changed url path

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+/* const path = require('path'); */
 const app = express();
 
 app.use(cors());
 
+app.use(express.static('./client/build/'));
+
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+
 const port = process.env.PORT || 4000;
-const path = require('path');
-let root = path.join(__dirname, 'client/build/');
-app.use(express.static(root));
-
-app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname, '/client/build/index.html'));
- })
-
 app.listen(port, () => console.log(`App is live on port ${port}!`));
