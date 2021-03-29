7e07d65434af071b0c385987cc1650572e314aaa
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2021-03-29 10:48:49 +0200

    changed express static again

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const app = express();
 app.use(cors());
 
 const path = require('path');
-app.use(express.static('client/build'));
-app.get('*', (req, res) => {
+/* app.use(express.static('client/build')); */
+
+app.use('/client', express.static(path.join(__dirname, 'build')))
+/* app.get('*', (req, res) => {
 	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
+}); */
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`App is live on port ${port}!`));
