d35dce339d2a437076b6be68c9aa26fc53651074
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2021-03-29 10:05:30 +0200

    added absolute path to express.static

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const app = express();
 
 app.use(cors());
 
-app.use(express.static('./client/build/'));
+/* app.use(express.static('build'));
 
 app.get('*', (req, res) => {
 	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
+}); */
+
+app.use('/client', express.static(path.join(__dirname, 'build')))
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`App is live on port ${port}!`));
