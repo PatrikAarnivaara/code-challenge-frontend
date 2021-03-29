bede64fc3b4b368acb9280ccd9d603f0bee2733a
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2021-03-29 10:16:01 +0200

    added app.get and sendFile pointing to path with build folder

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
+
 const app = express();
 
 app.use(cors());
 
-/* app.use(express.static('build'));
-
+const path = require('path');
+app.use('/client', express.static(path.join(__dirname, 'build')));
 app.get('*', (req, res) => {
 	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-}); */
-
-app.use('/client', express.static(path.join(__dirname, 'build')))
+});
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`App is live on port ${port}!`));
