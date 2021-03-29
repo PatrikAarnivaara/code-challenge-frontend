290444ccc1d5f76bc9a9edacaeb9ec6facb1aff7
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2021-03-29 11:41:33 +0200

    corrected path to build folder

--- server.js
+++ server.js
@@ -6,12 +6,11 @@ const app = express();
 app.use(cors());
 
 const path = require('path');
-/* app.use(express.static('client/build')); */
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, './client/build')));
 app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
+	res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`App is live on port ${port}!`));
