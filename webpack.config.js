8fb60ce4caa35b28baf9f9ffcbdc38a272b84683
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2021-04-05 13:58:17 +0200

    added missing dependency

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-  entry: './server.js',
-  mode: 'production',
-  target: 'node',
-  output: {
-    path: path.resolve(__dirname, '.'),
-    filename: 'server.bundle.js'
-  }
+	entry: './server.js',
+	mode: 'production',
+	target: 'node',
+	externals: [nodeExternals()],
+	output: {
+		path: path.resolve(__dirname, '.'),
+		filename: 'server.bundle.js',
+	},
 };
