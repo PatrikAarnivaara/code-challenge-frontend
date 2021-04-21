90d817af5f2a2e6c5eccec72e77454ea0d3a5199
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2021-04-21 18:42:08 +0200

    fixed bug in api request

--- server/client/src/shared/api/unsplash.jsx
+++ server/client/src/shared/api/unsplash.jsx
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 export default axios.create({
 	baseURL: 'https://api.unsplash.com/',
-	params: { per_page: 5, order_by: 'latest' },
-	headers: {
+/* 	params: { per_page: 5, order_by: 'latest' },
+ */	headers: {
 		Authorization: process.env.REACT_APP_UNSPLASH_CLIENT_ID,
 	},
 });
